fe01afbecea7a19c48a9050134a7b0d5b326c39d
bisonociudadjuanbosch <bisonociudadjuanbosch@cbisono.com>
2025-06-16 17:33:00 -0400

    ASA

--- Backend/Frontend/index.js
+++ Backend/Frontend/index.js
@@ -13,8 +13,9 @@ const Turno = require("./models/Turno");
 const app = express();
 
 // Ajustes para payload grande
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Hacer pública la carpeta de tickets
 const TICKETS_DIR = path.join(__dirname, "tickets");
